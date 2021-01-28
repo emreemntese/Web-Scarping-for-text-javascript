398b824f331735f8457add062d5e77355f70be70
Emre MENTEŞE <emrementese@gmail.com>
2021-01-28 18:40:28 +0300

    Update gettext.py

--- gettext.py
+++ gettext.py
@@ -1,3 +1,16 @@
-'''
-coming soon 
-'''
+import requests
+from bs4 import BeautifulSoup
+
+# scraping site
+url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+
+#we have 250 movies (data). we will extract relevant data from each movie's own page.
+
+allhtml = requests.get(url).content
+allhtml = BeautifulSoup(allhtml,"html.parser")
+
+# get the each data's url
+def geturl(allmovies):
+    pass
+
+print(allhtml)
